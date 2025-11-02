82bc45cf232920f9e6e84d88d94d07a8d2b32520
tunna <alnheae@gmail.com>
2025-11-03 06:20:43 +0700

    Minor adjustment in datatype for larger input

--- Number Theory/SquareNum.c
+++ Number Theory/SquareNum.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 
-int isSquare(int n){
+int isSquare(long long n){
     int squareRoot = sqrt(n);
-    if (n == pow(squareRoot, 2));
+    if (n == (long long)squareRoot * squareRoot)
     return 1;
+    return 0;
 }
 
 int main(){
+    int n; scanf("%d", &n);
+    if(isSquare(n)) printf("n is a Square number\n");
+    else printf("Not a square number\n");
 
     return 0;
 }
